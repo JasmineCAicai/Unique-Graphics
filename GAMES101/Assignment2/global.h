f4d05f7fe7d19cbe4e32e4d712194f7e59002734
JasmineCAicai <48902911+JasmineCAicai@users.noreply.github.com>
2021-08-09 12:57:32 +0800

    Upload Assignment 2

---
+++ GAMES101/Assignment2/global.h
@@ -0,0 +1,11 @@
+//
+// Created by 蔡淑娴 on 2021/8/9.
+//
+
+#ifndef ASSIGNMENT2_GLOBAL_H
+#define ASSIGNMENT2_GLOBAL_H
+
+//#define MY_PI 3.1415926
+//#define TWO_PI (2.0* MY_PI)
+
+#endif //ASSIGNMENT2_GLOBAL_H
