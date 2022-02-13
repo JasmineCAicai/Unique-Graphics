587fdd0ac670211066cf460daa471413d0ffba2e
JasmineCAicai <48902911+JasmineCAicai@users.noreply.github.com>
2022-02-13 09:59:17 +0800

    Upload Assignment 7

---
+++ GAMES101/Assignment7/build/CMakeFiles/Assignment7.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Assignment7"
+  "Assignment7.pdb"
+  "CMakeFiles/Assignment7.dir/BVH.cpp.o"
+  "CMakeFiles/Assignment7.dir/BVH.cpp.o.d"
+  "CMakeFiles/Assignment7.dir/Renderer.cpp.o"
+  "CMakeFiles/Assignment7.dir/Renderer.cpp.o.d"
+  "CMakeFiles/Assignment7.dir/Scene.cpp.o"
+  "CMakeFiles/Assignment7.dir/Scene.cpp.o.d"
+  "CMakeFiles/Assignment7.dir/Vector.cpp.o"
+  "CMakeFiles/Assignment7.dir/Vector.cpp.o.d"
+  "CMakeFiles/Assignment7.dir/main.cpp.o"
+  "CMakeFiles/Assignment7.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
