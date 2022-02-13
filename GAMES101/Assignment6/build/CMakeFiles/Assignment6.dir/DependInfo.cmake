4d33b73e7322342f92e7a9092575dcb180c6778c
JasmineCAicai <48902911+JasmineCAicai@users.noreply.github.com>
2022-02-13 09:48:26 +0800

    Upload Assignment 6

---
+++ GAMES101/Assignment6/build/CMakeFiles/Assignment6.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/caishuxian/Documents/Computer Graphics/GAMES101 - 现代计算机图形学入门/Assignment6/Assignment6/BVH.cpp" "CMakeFiles/Assignment6.dir/BVH.cpp.o" "gcc" "CMakeFiles/Assignment6.dir/BVH.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/GAMES101 - 现代计算机图形学入门/Assignment6/Assignment6/Renderer.cpp" "CMakeFiles/Assignment6.dir/Renderer.cpp.o" "gcc" "CMakeFiles/Assignment6.dir/Renderer.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/GAMES101 - 现代计算机图形学入门/Assignment6/Assignment6/Scene.cpp" "CMakeFiles/Assignment6.dir/Scene.cpp.o" "gcc" "CMakeFiles/Assignment6.dir/Scene.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/GAMES101 - 现代计算机图形学入门/Assignment6/Assignment6/Vector.cpp" "CMakeFiles/Assignment6.dir/Vector.cpp.o" "gcc" "CMakeFiles/Assignment6.dir/Vector.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/GAMES101 - 现代计算机图形学入门/Assignment6/Assignment6/main.cpp" "CMakeFiles/Assignment6.dir/main.cpp.o" "gcc" "CMakeFiles/Assignment6.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GAMES101/Assignment6/build/CMakeFiles/Assignment6.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Assignment6"
+  "Assignment6.pdb"
+  "CMakeFiles/Assignment6.dir/BVH.cpp.o"
+  "CMakeFiles/Assignment6.dir/BVH.cpp.o.d"
+  "CMakeFiles/Assignment6.dir/Renderer.cpp.o"
+  "CMakeFiles/Assignment6.dir/Renderer.cpp.o.d"
+  "CMakeFiles/Assignment6.dir/Scene.cpp.o"
+  "CMakeFiles/Assignment6.dir/Scene.cpp.o.d"
+  "CMakeFiles/Assignment6.dir/Vector.cpp.o"
+  "CMakeFiles/Assignment6.dir/Vector.cpp.o.d"
+  "CMakeFiles/Assignment6.dir/main.cpp.o"
+  "CMakeFiles/Assignment6.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
