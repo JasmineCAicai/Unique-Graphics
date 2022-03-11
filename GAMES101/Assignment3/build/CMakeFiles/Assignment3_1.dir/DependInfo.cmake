c293c8bd59bfe83b62906ebba7b4d6c7005fdc6c
JasmineCAicai <1105111422@qq.com>
2022-03-11 12:56:58 +0800

    Update

---
+++ GAMES101/Assignment3/build/CMakeFiles/Assignment3_1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/caishuxian/Documents/Computer Graphics/Unique-Graphics/GAMES101/Assignment3/Texture.cpp" "CMakeFiles/Assignment3_1.dir/Texture.cpp.o" "gcc" "CMakeFiles/Assignment3_1.dir/Texture.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/Unique-Graphics/GAMES101/Assignment3/Triangle.cpp" "CMakeFiles/Assignment3_1.dir/Triangle.cpp.o" "gcc" "CMakeFiles/Assignment3_1.dir/Triangle.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/Unique-Graphics/GAMES101/Assignment3/main.cpp" "CMakeFiles/Assignment3_1.dir/main.cpp.o" "gcc" "CMakeFiles/Assignment3_1.dir/main.cpp.o.d"
+  "/Users/caishuxian/Documents/Computer Graphics/Unique-Graphics/GAMES101/Assignment3/rasterizer.cpp" "CMakeFiles/Assignment3_1.dir/rasterizer.cpp.o" "gcc" "CMakeFiles/Assignment3_1.dir/rasterizer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GAMES101/Assignment3/build/CMakeFiles/Assignment3_1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "Assignment3_1"
+  "Assignment3_1.pdb"
+  "CMakeFiles/Assignment3_1.dir/Texture.cpp.o"
+  "CMakeFiles/Assignment3_1.dir/Texture.cpp.o.d"
+  "CMakeFiles/Assignment3_1.dir/Triangle.cpp.o"
+  "CMakeFiles/Assignment3_1.dir/Triangle.cpp.o.d"
+  "CMakeFiles/Assignment3_1.dir/main.cpp.o"
+  "CMakeFiles/Assignment3_1.dir/main.cpp.o.d"
+  "CMakeFiles/Assignment3_1.dir/rasterizer.cpp.o"
+  "CMakeFiles/Assignment3_1.dir/rasterizer.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment3_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
